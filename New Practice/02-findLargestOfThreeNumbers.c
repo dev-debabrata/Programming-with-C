b60c7acc11a91d5d97d0e1b2c4a1e9884336ef87
Debabrata Das <debabratadas711@gmail.com>
2026-02-15 22:24:00 +0530

    15-02-2026

---
+++ New Practice/02-findLargestOfThreeNumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num1, num2, num3;
+
+    printf("Enter the three numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    if(num1 > num2 && num1 > num3){
+        printf("%d is a Largest number.", num1);
+    } else if(num2 > num1 && num2 > num3){
+        printf("%d is a Largest number.", num2);
+    } else{
+        printf("%d is a Largest number.", num3);
+    }
+    return 0;
+}
