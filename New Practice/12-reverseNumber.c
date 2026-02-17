087f55407d806017e21cb2ae5ed1e666b4c067f8
Debabrata Das <debabratadas711@gmail.com>
2026-02-17 22:49:10 +0530

    17-02-2026

---
+++ New Practice/12-reverseNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int num, rev = 0, rem;
+
+    printf("Enter the numbers: ");
+    scanf("%d", &num);
+
+    while(num != 0){
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reverse numbers: %d", rev);
+    return 0;
+}
+
